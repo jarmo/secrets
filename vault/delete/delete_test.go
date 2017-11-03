66137332f5a28ef8c125a43972f235c0986b429c
Jarmo Pertman <jarmo.p@gmail.com>
2017-11-03 12:26:20 +0200

    Add first test.

---
+++ vault/delete/delete_test.go
@@ -0,0 +1,26 @@
+package delete
+
+import (
+  "testing"
+  "fmt"
+  "github.com/jarmo/secrets/secret"
+)
+
+func TestExecute(t *testing.T) {
+  secret1 := secret.New("name-1", "value-1")
+  secret2 := secret.New("name-2", "value-2")
+  secret3 := secret.New("name-3", "value-3")
+  secrets := [...]secret.Secret{secret1, secret2, secret3}
+
+  deletedSecret, newSecrets := Execute(secrets[:], 1)
+
+  if deletedSecret != secret2 {
+    t.Fatal(fmt.Sprintf("Expected %s to be deleted, but was %s", secret2, deletedSecret))
+  }
+
+  expectedNewSecrets := [...]secret.Secret{secret1, secret3}
+
+  if fmt.Sprintf("%v", newSecrets) != fmt.Sprintf("%v", expectedNewSecrets) {
+    t.Fatal(fmt.Sprintf("Expected new secrets to be %s, but got %s", expectedNewSecrets, newSecrets))
+  }
+}
