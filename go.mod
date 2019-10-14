83e4bfd646b2c35f6f52f61471aed2bf48866dc6
Jarmo Pertman <jarmo.p@gmail.com>
2019-10-14 16:54:07 +0300

    Remove path functions form core which do not need to be shared

--- go.mod
+++ go.mod
@@ -4,7 +4,6 @@ go 1.12
 
 require (
 	github.com/kr/pretty v0.1.0 // indirect
-	github.com/pinzolo/xdgdir v0.0.0-20180220135956-c5b4bdb0a9c4
 	github.com/satori/go.uuid v1.2.0
 	golang.org/x/crypto v0.0.0-20181112202954-3d3f9f413869
 	golang.org/x/sys v0.0.0-20181107165924-66b7b1311ac8 // indirect
--- storage/path/path.go
+++ storage/path/path.go
@@ -5,38 +5,11 @@ import (
   "io/ioutil"
   "encoding/json"
   "errors"
-  "github.com/pinzolo/xdgdir"
 )
 
 type Config struct {
   Path string
   Alias string
-}
-
-func Get(alias string) (string, error) {
-  if configs, err := Configurations(configurationPath()); err == nil {
-    if confByAlias := FindByAlias(configs, alias); confByAlias != nil {
-      return confByAlias.Path, nil
-    } else {
-      return configs[0].Path, nil
-    }
-  } else {
-    return "", err
-  }
-}
-
-func Store(vaultPath string, vaultAlias string) string {
-  configurationPath := configurationPath()
-  conf, _ := Configurations(configurationPath)
-  conf = append(conf, Config{Path: vaultPath, Alias: vaultAlias})
-
-  if configJSON, err := json.MarshalIndent(conf, "", " "); err != nil {
-    panic(err)
-  } else if err := ioutil.WriteFile(configurationPath, configJSON, 0600); err != nil {
-    panic(err)
-  }
-
-  return configurationPath
 }
 
 func Configurations(path string) ([]Config, error) {
@@ -62,12 +35,3 @@ func FindByAlias(configs []Config, alias string) *Config {
   return nil
 }
 
-func configurationPath() string {
-  xdgApp := xdgdir.NewApp("secrets")
-  xdgConfigurationFilePath, err := xdgApp.ConfigFile("config.json")
-  if err != nil {
-    panic(err)
-  }
-
-  return xdgConfigurationFilePath
-}
