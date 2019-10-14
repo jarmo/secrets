abfed23becc907b9a4f5479cac7d2033c6722a07
Jarmo Pertman <jarmo.p@gmail.com>
2019-10-14 20:01:41 +0300

    Generify path for reuse between cli and web

--- storage/path/path.go
+++ storage/path/path.go
@@ -7,25 +7,50 @@ import (
   "errors"
 )
 
-type Config struct {
+type config struct {
   Path string
   Alias string
 }
 
-func Configurations(path string) ([]Config, error) {
-  if configJSON, err := ioutil.ReadFile(path); os.IsNotExist(err) {
-    return make([]Config, 0), errors.New("Vault is not configured!")
+func Get(configurationPath, alias string) (string, error) {
+  if configs, err := configurations(configurationPath); err == nil {
+    if confByAlias := findByAlias(configs, alias); confByAlias != nil {
+      return confByAlias.Path, nil
+    } else {
+      return "", errors.New("Vault not found!")
+    }
   } else {
-    var conf []Config
+    return "", err
+  }
+}
+
+func Store(configurationPath, vaultPath, vaultAlias string) string {
+  conf, _ := configurations(configurationPath)
+  conf = append(conf, config{Path: vaultPath, Alias: vaultAlias})
+
+  if configJSON, err := json.MarshalIndent(conf, "", " "); err != nil {
+    panic(err)
+  } else if err := ioutil.WriteFile(configurationPath, configJSON, 0600); err != nil {
+    panic(err)
+  }
+
+  return configurationPath
+}
+
+func configurations(path string) ([]config, error) {
+  if configJSON, err := ioutil.ReadFile(path); os.IsNotExist(err) {
+    return make([]config, 0), errors.New("Vault is not configured!")
+  } else {
+    var conf []config
     if err := json.Unmarshal(configJSON, &conf); err == nil {
       return conf, nil
     } else {
-      return make([]Config, 0), err
+      return make([]config, 0), err
     }
   }
 }
 
-func FindByAlias(configs []Config, alias string) *Config {
+func findByAlias(configs []config, alias string) *config {
   for _, config := range configs {
     if config.Alias == alias {
       return &config
